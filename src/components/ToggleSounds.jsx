8c57630d208b43ab4bf3ec32a23a1803d8982135
Leonardo da Vinci <leonardodavinci2049@gmail.com>
2024-09-02 08:44:49 -0300

    Start project

---
+++ src/components/ToggleSounds.jsx
@@ -0,0 +1,20 @@
+import PropTypes from 'prop-types';
+
+function ToggleSounds({ allowSound, setAllowSound }) {
+  return (
+    <button
+      className="btn-sound"
+      onClick={() => setAllowSound((allow) => !allow)}
+    >
+      {allowSound ? "🔈" : "🔇"}
+    </button>
+  );
+}
+
+ToggleSounds.propTypes = {
+  allowSound: PropTypes.bool.isRequired,
+  setAllowSound: PropTypes.func.isRequired,
+
+};
+
+export default ToggleSounds;
